593996689268e66709f9b1911dfd9f3e9716510e
fabiodelli <ntqdel@hotmail.com>
2023-03-22 17:07:11 +0100

    add milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
+/* MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+ */
+
 let team = [
     {
       nome: 'Wayne Barnett',
       ruolo: 'Founder & CEO',
-      foto: 'wayne-barnett-founder-ceo.jpg'
+      foto: './assets/img/wayne-barnett-founder-ceo.jpg'
     },
     {
       nome: 'Angela Caroll',
       ruolo: 'Chief Editor',
-      foto: 'angela-caroll-chief-editor.jpg'
+      foto: './assets/img/angela-caroll-chief-editor.jpg'
     },
     {
       nome: 'Walter Gordon',
@@ -17,17 +21,39 @@ let team = [
     {
       nome: 'Angela Lopez',
       ruolo: 'Social Media Manager',
-      foto: 'angela-lopez-social-media-manager.jpg'
+      foto: './assets/img/angela-lopez-social-media-manager.jpg'
     },
     {
       nome: 'Scott Estrada',
       ruolo: 'Developer',
-      foto: 'scott-estrada-developer.jpg'
+      foto: './assets/img/scott-estrada-developer.jpg'
     },
     {
       nome: 'Barbara Ramos',
       ruolo: 'Graphic Designer',
-      foto: 'barbara-ramos-graphic-designer.jpg'
+      foto: '.barbara-ramos-graphic-designer.jpg'
     }
   ];
-  
+
+/*   MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto */  
+
+    for (let i = 0; i < team.length; i++) {
+
+        console.log(`Nome: ${team[i].nome}, Ruolo: ${team[i].ruolo}, Foto: ${team[i].foto}`);
+    }
+
+/* MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+ */
+
+    const teamMembersDiv = document.getElementById("team-members");
+
+    for (let i = 0; i < team.length; i++) {
+        const memberDiv = document.createElement("div");
+        memberDiv.innerHTML =`Nome: ${team[i].nome}, Ruolo: ${team[i].ruolo}, Foto: ${team[i].foto}`;
+        teamMembersDiv.appendChild(memberDiv)
+    }
+
+    
+
